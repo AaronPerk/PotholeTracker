694150b9fa5ea4987b95b66896d98d63b5b13ea2
Subhadra Pullabhatla <perkinsa3@udayton.edu>
2017-04-20 15:23:07 -0400

    Revert "removed empty tests"
    This reverts commit 244c90222d1c4f6f61a3f7c476f2b30c829b318b.

---
+++ Capstone.Web.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Capstone.Web.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void HomeController_IndexAction_ReturnIndexView()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
